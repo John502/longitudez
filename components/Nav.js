5c398df17032197e830c731205d842622ce0cd35
jmurnerGeoComm <jmurner@geo-comm.com>
2021-05-09 23:22:56 -0400

    adding a menu

--- components/Nav.js
+++ components/Nav.js
@@ -13,6 +13,16 @@ const Nav = () => {
 					<span></span>
 				</div>
             </div>
+            <div className={headerSty.sidebar}>
+                <ul className={headerSty.sidebarList}>
+                    <li>Home</li>
+                    <l1>Maps</l1>
+                    <li>Data</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+
         </nav>
     )
 }
