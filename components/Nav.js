ec9c4ae7732709b74196062803637fa198331d15
jmurnerGeoComm <jmurner@geo-comm.com>
2021-05-21 00:36:27 -0400

    hamburger x

--- components/Nav.js
+++ components/Nav.js
@@ -3,26 +3,21 @@ import navSty from '../styles/Nav.module.css'
 
 const Nav = () => {
   return (
-      <>
+    <>
+      <div className={navSty.container}>
         <nav role="navigation" className={navSty.nav}>
-        <div className={navSty.navitems}>
         <div className={navSty.logo}>longitudez</div>
         <div className={navSty.inputContainer}>
             <input type="checkbox" id={navSty.tog}></input>
-            <div className={navSty.hamburger}>
                 <span></span>
                 <span></span>
                 <span></span>
-            </div>
-        </div>
         </div>
         </nav>
         <div className={navSty.sidemenu}>
-            <ul className={navSty.sidemenuList}>
-            <li>Home</li>
-            <li>Contact</li>
-         </ul>
+
         </div>
+    </div>
     </>
   )
 }
