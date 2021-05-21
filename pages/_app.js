51faf29c595f89721b540a9427ae359e8fade3db
jmurnerGeoComm <jmurner@geo-comm.com>
2021-05-20 21:15:40 -0400

    display content

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,7 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   
-  return ( <Layout><Component {...pageProps} /> </Layout>
-  )
+  return (<><Layout></Layout><Component {...pageProps} /></>)
   
 }
 
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import aboutSty from '../styles/About.module.css'
+
+
+export default function About() {
+    return (
+       <div className={aboutSty.container}>
+         <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto dolor pariatur, alias dignissimos quaerat accusamus saepe delectus distinctio porro soluta quis! Exercitationem ab voluptates eligendi commodi cumque consequuntur et ipsa.</h1>
+       </div>
+    )
+  }
+  
