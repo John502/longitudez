a34b2200ca01a539e4fdfd7c416ccc906fa0ee51
jmurnerGeoComm <jmurner@geo-comm.com>
2021-05-23 22:29:17 -0400

    Title and Stuff

--- components/Layout.js
+++ components/Layout.js
@@ -3,14 +3,28 @@ import Nav from './Nav'
 import styles from '../styles/Layout.module.css'
 
 
-const Layout = ({children}) => {
+const Layout = ({children, title, keywords, description}) => {
     return (
         <>
-        <Head></Head>
+        <Head>
+            <meta name='viewport' content='width=device-width, initial-scale=1' />
+            <meta name='keywords' content={keywords} />
+            <meta name='description' content={description} />
+            <meta charSet='utf-8' />
+            <link rel='icon' href='/favicon.ico' />
+            <title>{title}</title>
+        </Head>
         <Nav></Nav>
 
         </>
     )
+}
+
+Layout.defaultProps = {
+    title: 'Longitudez',
+    keywords: 'Maps, GIS, Mapping',
+    description: 'Adventures in GIS and mapping'
+
 }
 
 export default Layout
