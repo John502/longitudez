34e8c704932fc761f25cbde2bbfe42bd334f1b80
jmurnerGeoComm <jmurner@geo-comm.com>
2021-05-25 00:08:27 -0400

    Automatically redirect user

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import NotFoundsty from '../styles/NotFound.module.css'
 import Link from 'next/link'
 
 export default function Custom404() {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+        router.push('/')
+        }, 3000)
+
+    })
     return (
     <div className={NotFoundsty.container}>
     <div id={NotFoundsty.card}>
         <h1>404 - Page Not Found</h1>
         <h2>Bummer!</h2>
-        <Link href="/"><a id={NotFoundsty.homelink}>Home</a></Link>
+        <Link href="/"><a id={NotFoundsty.homelink}>Go back Home</a></Link>
         </div>
     </div>)
   }
