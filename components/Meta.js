12f5edf853d3fa43815cd24b08d2847220dbe381
John502 <john.murner@gmail.com>
2021-10-02 23:56:35 -0400

    Viewport and zoom level

--- components/Meta.js
+++ components/Meta.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
 const Meta = ({ title, keywords, description }) => {
     return (
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name='keywords' content={keywords} />
         <meta name='description' content={description} />
         <meta charSet='utf-8' />
