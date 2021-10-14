9605135178d4f980396a083750d150d6d3e08f07
John502 <john.murner@gmail.com>
2021-10-14 14:30:56 -0400

    Map card display

--- mapdata/maps.js
+++ mapdata/maps.js
@@ -1,15 +1,17 @@
  export const maps =   [
     {
         "id": 1,
-        "slug": "Louisville Nature Center",
-        "image": "./images/maps/001_LouisvilleNatureCenter.jpg",
-        "date": "October 13 2021"
+        "slug": "Louisville_Nature_Center",
+        "title": "Louisville Nature Center",
+        "cover_image": "./images/maps/001_LouisvilleNatureCenter.jpg",
+        "date": "October 12 2021"
     
     },
     {
         "id": 2,
-        "slug": "Tioga Falls",
-        "image": "./images/maps/002_TiogaFalls_200_200.png",
+        "slug": "Tioga_Falls",
+        "title": "Tioga Falls",
+        "cover_image": "./images/maps/002_TiogaFalls_200_200.png",
         "date": "October 13 2021"
     }
 ]
--- pages/maps.js
+++ pages/maps.js
@@ -1,33 +1,35 @@
 import { server } from '../config'
+import { sortByDateMaps } from '../utils'
 import Meta from "../components/Meta";
+import Card from "../components/map/card"
+import sty from "../styles/Maps.module.css"
 
 export default function Maps({maps}){
-
-  // const LeafletMap = dynamic(
-  //   () => {
-  //     return import("../components/lf/LeafletMap");
-  //   },
-  //   { ssr: false }
-  // );
 
     return (
     <>
     <Meta title={Meta.defaultProps.title + ' | Maps'} />
-            <div>files</div>
+    <div className={sty.container}>
+            <div className={sty.header}>Maps</div>
+            <div className={sty.cards}>
+      {maps.map((post, index) => (
+          <Card key={index} post={post}></Card>
+        ))}
+    </div>
+    </div>
     </>
     )
 }
 
   
 export const getStaticProps = async () => {
+  
   const res = await fetch(`${server}/api/maps`)
   const maps = await res.json()
 
-  console.log(maps)
-
   return {
     props: {
-      maps,
+      maps: maps.sort(sortByDateMaps),
     },
   }
 }
