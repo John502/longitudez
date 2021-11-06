485031c4f9dca597a6845b5daab2f77908e0e9e4
John502 <john.murner@gmail.com>
2021-11-06 19:05:43 -0400

    Reverting to full screen map

--- components/lf/LeafletMap.js
+++ components/lf/LeafletMap.js
@@ -2,25 +2,22 @@ import { MapContainer, TileLayer, GeoJSON} from "react-leaflet";
 import LeafletLocateButton from "./LeafletLocateButton";
 import styles from '../../styles/LeafletMap.module.css'
 import "leaflet/dist/leaflet.css"
-import { map } from "leaflet";
 
 const LeafletMap = ({mapData}) => {
 
 
     return (
-      <div className={styles.container}>
           <MapContainer
           center={mapData.defaultPosition}
           zoom={mapData.defaultZoom} 
           tap={false}
-          className={styles.mapContainer}>
+          className={styles.container}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/> 
         <LeafletLocateButton 
           title={"+"}/>
         </MapContainer>
-        </div>
     )
 }
 
