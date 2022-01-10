cc19bb56c77a463b8045b559e28231f2c98e02e0
John502 <john.murner@gmail.com>
2022-01-09 21:16:13 -0500

    Updating Tioga Falls Maps

--- components/lf/LeafletMap.js
+++ components/lf/LeafletMap.js
@@ -9,7 +9,6 @@ import L from "leaflet";
 
 const LeafletMap = ({mapData}) => {
 
-    // const geoJsonBounds = L.geoJSON(mapData.geo).getBounds()
     
     const imageBounds = [ [mapData.map_png_bnds.Ymin, mapData.map_png_bnds.Xmin], 
                           [mapData.map_png_bnds.Ymax, mapData.map_png_bnds.Xmax]]
@@ -21,34 +20,26 @@ const LeafletMap = ({mapData}) => {
             tap={false}
             className={styles.mapcontainer}>
               <LayersControl position="topleft">
-      <LayersControl.Overlay checked="true" name="Longitudez">
-        <ImageOverlay attribution={mapData.map_png_attr}
-            url={mapData.map_png} bounds={imageBounds}>
-        </ImageOverlay>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="GPS">
-        <GeoJSON key={mapData.slug} data={mapData.geo}></GeoJSON>
-      </LayersControl.Overlay>
-      <LayersControl.BaseLayer checked name="USGS Topo">
-        <TileLayer
-          url="https://basemap.nationalmap.gov/ArcGIS/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}"
-        />
-      </LayersControl.BaseLayer>
-      <LayersControl.BaseLayer name="OSM">
-        <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/> 
-      </LayersControl.BaseLayer>
-    </LayersControl>
-          {/* <ImageOverlay attribution={mapData.map_png_attr}
-            url={mapData.map_png} bounds={imageBounds}>
-            </ImageOverlay> */}
-            <LeafletLocateButton 
-          title={"+"}/>
-          {/* <GeoJSON key={mapData.slug} data={mapData.geo}></GeoJSON>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>  */}
+                <LayersControl.Overlay checked="true" name="Longitudez">
+                  <ImageOverlay attribution={mapData.map_png_attr}
+                      url={mapData.map_png} bounds={imageBounds}>
+                  </ImageOverlay>
+                </LayersControl.Overlay>
+              <LayersControl.Overlay name="GPS">
+                <GeoJSON key={mapData.slug} data={mapData.geo}></GeoJSON>
+              </LayersControl.Overlay>
+              <LayersControl.BaseLayer checked name="USGS Topo">
+                <TileLayer
+                  url="https://basemap.nationalmap.gov/ArcGIS/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}"
+                />
+              </LayersControl.BaseLayer>
+              <LayersControl.BaseLayer name="OSM">
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/> 
+              </LayersControl.BaseLayer>
+            </LayersControl>
+            <LeafletLocateButton title={"+"}/>
         </MapContainer>
     )
 }
