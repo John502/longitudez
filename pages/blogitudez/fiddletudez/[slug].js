73a10aea202f5d16d862036088540f9c3569f477
John502 <john.murner@gmail.com>
2022-10-18 18:22:54 -0400

    Displaying fiddle tune chords

--- pages/blogitudez/fiddletudez/[slug].js
+++ pages/blogitudez/fiddletudez/[slug].js
@@ -3,25 +3,26 @@ import Meta from '../../../components/Meta'
 
 import sty from '../../../styles/Blogpost.module.css'
 import {tunesData} from '../../../data/tunes'
-
+import Staff from '../../../components/tunes/staff'
 
 export default function TunePost({slug, title, arrangements})
 {
   const arr = arrangements[0]
-  const staffs = arr.staffs 
-  console.log(arr.staffs.A1)
-  
+
   return  <>
            <Meta title={Meta.defaultProps.title + ' | ' + title} />
            <Meta description={title} />
           <div className={sty.container}>
           <article className={sty.content}>
              <h1>{title}</h1>
-             <h2>Key: {arr.key}</h2>
-             <div>A1 : {arr.staffs.A1}</div>
-             <div>A2 : {arr.staffs.A2}</div>
-             <div>B1 : {arr.staffs.B1}</div>
-             <div>B2 : {arr.staffs.B2}</div>
+             <h2>Key : {arr.key}</h2>
+             {arr.staffs.map((staff, index) => (
+              <Staff staff={Object.entries(staff)} ></Staff>
+             ))}
+             {/* <Staff staff={arr.staffs.A1} title='A1'></Staff> */}
+             {/* <div>A2 : {arr.staffs[1]}</div>
+             <div>B1 : {arr.staffs[2]}</div>
+             <div>B2 : {arr.staffs[3]}</div> */}
         </article>
         </div>
           </>
