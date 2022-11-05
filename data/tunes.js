edbc6efb9f1a7108b03e859d6e5d4ae41e4c71cf
John502 <john.murner@gmail.com>
2022-11-05 00:19:28 -0400

    More tunes

--- data/tunes.js
+++ data/tunes.js
@@ -3,10 +3,30 @@ export const tunesData = [
        slug: "Louisville_Breakdown_G",
        title: "Louisville Breakdown",
        chords: "/images/tunechords/LouisvilleBreakdown.svg",
-     },
+       key: "G",
+      },
      {
       slug: "Boatin_Up_Sandy_A",
       title: "Boatin Up Sandy",
       chords: "/images/tunechords/BoatinUpSandy.svg",
-    } 
+      key: "Amix"
+    },
+    {
+      slug: "Hog_Eyed_Man_Amix",
+      title: "Hog Eyed Man",
+      chords: "/images/tunechords/HogEyedMan.svg",
+      key: "Amix"
+    }, 
+    {
+      slug: "Waynesboro_A",
+      title: "Waynesboro",
+      chords: "/images/tunechords/Waynesboro.svg",
+      key: "A"
+    }, 
+    {
+      slug: "Waynesboro_Mobile",
+      title: "Waynesboro Mobile",
+      chords: "/images/tunechords/Waynesboro_Mobile.svg",
+      key: "A"
+    }, 
 ];
